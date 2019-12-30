a9704d5ccf0ead7d22978db4d4f8045ad2fd6e94
Persevere Von <wu543065657@163.com>
2019-12-30 11:43:14 +0800

    feat: Add alacritty configuration file.

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ function doIt() {
     ln -sb ~/.dotfiles/.config/nvim ~/.config/nvim
     ln -sb ~/.dotfiles/.tmux.conf ~/.tmux.conf
     ln -sb ~/.dotfiles/.zshrc ~/.zshrc
+    ln -sb ~/.dotfiles/.config/alacritty ~/.config/alacritty
 }
 
 doIt
