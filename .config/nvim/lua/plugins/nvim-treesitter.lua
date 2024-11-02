3e20e5fbe832337679fd0b0e62ab8ba7766548c8
Dezhi Wu <hi@5xx.io>
2024-11-02 11:41:43 +0800

    Update Neovim plugins configuration

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -6,7 +6,7 @@ return {
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+      ensure_installed = { "bash", "c", "diff", "html", "javascript", "json", "jsonc", "jsonnet", "lua", "markdown", "markdown_inline", "query", "rust", "terraform", "toml", "typescript", "vim", "vimdoc", "yaml"},
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 vim.keymap.set('n', '<C-p>', '<cmd>Telescope find_files<cr>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>b', '<cmd>Telescope buffers<cr>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>tg', '<cmd>Telescope treesitter<cr>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>tg', '<cmd>Telescope lsp_document_symbols<cr>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>m', '<cmd>Telescope oldfiles<cr>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>a', '<cmd>Telescope live_grep<cr>', { noremap = true, silent = true })
 
